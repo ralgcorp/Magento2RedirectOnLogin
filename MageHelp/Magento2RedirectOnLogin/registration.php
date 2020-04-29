51dbbe134ec93245eddaceaddd2e669133c81bf3
MageHelp <46701434+ralgcorp@users.noreply.github.com>
2020-04-29 07:00:27 -0300

    Add files via upload

---
+++ MageHelp/Magento2RedirectOnLogin/registration.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @author Rodrigo Gabriel <ralgcorp@gmail.com>
+ * Date: 24/02/2020
+ * Time: 15:50
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'MageHelp_Magento2RedirectOnLogin',
+    __DIR__
+);
